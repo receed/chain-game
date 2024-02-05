b4defdc8a7a39d3f986363a71a471c9a72f9652a
Andrey Efremov <efremov.ap@phystech.edu>
2024-02-05 12:47:34 +0300

    Add graphics

--- process.js
+++ process.js
@@ -1,8 +1,16 @@
 const MAX_VALUE = 100
 const HIT_COUNT = 3
+const ITEMS = 24
+const SEGMENT_ANGLE = Math.PI * 2 / ITEMS;
+
+let segments = undefined
 
 function getRandomInt(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function getRandomReal() {
+    return getRandomInt(1, 300) / 10
 }
 
 function bernoulli(p) {
@@ -36,11 +44,11 @@ function generate(acc) {
 			else if (sign == '+')
 				lowerBound = 5
 		}
-		let b = getRandomInt(lowerBound, 9)
+		let b = localStorage["level"] == "2" ? getRandomReal() : getRandomInt(lowerBound, 9)
 		localStorage["sign"] = sign
 		localStorage["b"] = b
 		let result = eval(`${acc} ${sign} ${b}`)
-		if (!(result >= 0 && result == Math.floor(result) && result <= MAX_VALUE))
+		if (localStorage["level"] == "1" && !(result >= 0 && result == Math.floor(result) && result <= MAX_VALUE))
 			continue
 		if (result == acc || result == 0 || result == 1) {
 			if (localStorage["lastStupid"] < 6) {
@@ -60,8 +68,14 @@ function generate(acc) {
 			sign = '×'
 		else if (sign == '/')
 			sign = ':'
+		result = Math.round(result * 100) / 100
 		return [`${sign} ${b}`, `${acc} ${sign} ${b} = `, result]
 	}
+}
+
+function rotateSegments() {
+	segments.unshift(segments.pop())
+	segments[10].children[0].innerHTML = ""
 }
 
 function updateExpr() {
@@ -69,6 +83,9 @@ function updateExpr() {
 	localStorage["exprRight"] = exprRight
 	localStorage["exprStr"] = exprStr
 	localStorage["result"] = result
+	rotateSegments()
+	segments[0].children[0].innerText = exprRight
+	rotateSegments()
 }
 
 function setResult(res) {
@@ -78,7 +95,7 @@ function setResult(res) {
 		elem.className = "correct"
 		document.getElementById("hp").className = "correct"
 	} else if (res == "fail") {
-		elem.innerText = "Неправильно"
+		elem.innerText = "Неправильно" + (localStorage["failInfo"] ?? "")
 		elem.className = "wrong"
 		document.getElementById("hp").className = "wrong"
 	} else {
@@ -102,13 +119,13 @@ function priority(sign) {
 
 function check() {
 	let answer = document.getElementById("answer").value
+	answer = parseFloat(answer.replace(',', '.'))
 	let result = localStorage["result"]
 	let hitCount = localStorage["hitCount"]
 	let score = Number(localStorage["score"])
 	if (answer == result) {
 		localStorage["checkResult"] = "ok"
 		let longExpr = localStorage["longExpr"]
-		// console.log(longExpr)
 		if (!longExpr)
 			longExpr = localStorage["acc"].toString()
 		let sign = localStorage["sign"]
@@ -131,23 +148,61 @@ function check() {
 			localStorage["bestScore"] = score
 		}
 		document.getElementById("answer").value = ""
+		// document.getElementById("expr-next").innerHTML = ""
+		let wheel = document.querySelector(".wheel-inner")
+		trans = wheel.style.transform
+		let angle = 0
+		let match = trans.match(/rotateX\((.*)rad\)/)
+		if (match) {
+			angle = parseFloat(match[1])
+		}
+		angle += SEGMENT_ANGLE * 2
+		wheel.style.transform = `rotateX(${angle}rad)`
+		trans = window.getComputedStyle(wheel).transform
 		updateExpr()
 	} else {
 		hitCount--
 		localStorage["hitCount"] = hitCount
 		localStorage["checkResult"] = "fail"
+		localStorage["failInfo"] = ""
 		if (hitCount == 0) {
 			localStorage["score"] = 0
-			document.getElementById("ok").innerText += `, хиты закончились. Очки: ${score}. ${document.getElementById("expr").innerText} ${result}`
+			localStorage["failInfo"] = `, хиты закончились. Очки: ${score}. ${document.getElementById("expr").innerText} ${result}`
+			console.log("text: " + document.getElementById("ok").innerText)
 			resetGame()
 		}
 	}
 	redraw()
 }
 
+function setLevel(levelId, levelName) {
+	localStorage["level"] = levelId
+	window.confirm(`Сменить уровень на ${levelName}? Текущий прогресс будет потерян`)
+	resetGame()
+	redraw()
+}
+
+function resetWheel() {
+	const wheel = document.querySelector(".wheel .wheel-inner")
+	const diameter = wheel.clientHeight
+	const radius = diameter / 2;
+	const height = radius * Math.tan(SEGMENT_ANGLE / 2) * 2
+	wheel.style.transform = `rotateX(0rad)`
+	segments.forEach((segment, i) => {
+		let transform = `rotateX(${ SEGMENT_ANGLE * (i - 1) }rad) translateZ(${ radius }px)`
+		segment.style.height = `${height}px`	
+		segment.style.transform = transform
+		segment.children[0].innerHTML = ""
+	})
+}
+
 function resetGame() {
+	resetWheel()
+	const wheel = document.querySelector(".wheel .wheel-inner")
+	wheel.style.transform = `rotateX(${SEGMENT_ANGLE * 2}rad)`
+	localStorage["level"] = localStorage["level"] ?? "1"
 	localStorage["prevAcc"] = ""
-	localStorage["acc"] = getRandomInt(1, 9)
+	localStorage["acc"] = localStorage["level"] == "2" ? getRandomReal() : getRandomInt(1, 9)
 	localStorage["longExpr"] = ""
 	localStorage["displayedLongExpr"] = ""
 	localStorage["lastSign"] = ""
@@ -155,20 +210,33 @@ function resetGame() {
 	localStorage["hitCount"] = HIT_COUNT
 	localStorage["lastStupid"] = 0
 	localStorage["simpleCount"] = 0
+	segments[0].children[0].innerHTML = localStorage["acc"]
 	updateExpr()
 	document.getElementById("answer").value = ""
 }
 
+function loadWheel() {
+	let values = JSON.parse(localStorage["segments"])
+	for (let i = 0; i < segments.length; i++) {
+		segments[i].children[0].innerHTML = values[i]
+	}
+}
+
+function saveWheel() {
+	localStorage["segments"] = JSON.stringify(segments.map((s) => s.children[0].innerHTML))
+}
+
 function redraw() {
+	saveWheel();
 	document.getElementById("hp").innerText = localStorage["hitCount"]
 	document.getElementById("hp-max").innerText = HIT_COUNT
 	document.getElementById("best-score").innerText = localStorage["bestScore"] ?? 0
 	document.getElementById("long-expr").innerText = localStorage["displayedLongExpr"]
 	document.getElementById("score").innerText = localStorage["score"]
 	document.getElementById("expr").textContent = localStorage["exprStr"]
-	document.getElementById("expr-prev").textContent = localStorage["prevAcc"]
-	document.getElementById("expr-left").textContent = localStorage["acc"]
-	document.getElementById("expr-right").textContent = localStorage["exprRight"]
+	// document.getElementById("expr-prev").textContent = localStorage["prevAcc"]
+	// document.getElementById("expr-left").textContent = localStorage["acc"]
+	// document.getElementById("expr-right").textContent = localStorage["exprRight"]
 	setResult(localStorage["checkResult"])
 }
 
@@ -177,15 +245,14 @@ function setup() {
 	if (inIframe) {
 		document.getElementById("top-nav").style.display = "none"
 	}
-	if (!Number(localStorage["hitCount"])) {
-		resetGame()
-		localStorage["checkResult"] = ""
-	}
-	redraw()
 	document.getElementById("check").addEventListener("click", check)
 	document.getElementById("answer").addEventListener("keyup", (event) => {
 		if (event.keyCode == 13)
 			check()
+	})
+	document.getElementById("answer").addEventListener("input", (event) => {
+		// document.getElementById("expr-next").innerHTML = event.target.value;
+		segments[0].children[0].innerHTML = event.target.value;
 	})
 	document.getElementById("restart").onclick = function () {
 		resetGame()
@@ -196,6 +263,12 @@ function setup() {
 		localStorage["bestScore"] = 0
 		redraw()
 	}
+
+	document.querySelectorAll("input[name='level']").forEach((input) => {
+		input.onclick = (event) => {
+			setLevel(event.target.id, event.target.parentElement.innerText)
+		}
+	})
 
 	if (!inIframe && false) {
 		// let host = "file:///home/receed/prog/game/"
@@ -225,7 +298,23 @@ function setup() {
 			})
 		})
 	}
+	const wheel = document.querySelector(".wheel .wheel-inner")
+	let added = ITEMS - wheel.childElementCount
+	for (let i = 0; i < added; i++) {
+		wheel.appendChild(wheel.children[0].cloneNode(true))
+	}
+	segments = Array.from(wheel.children)
+
+	if (Number(localStorage["hitCount"])) {
+		resetWheel()
+		loadWheel()
+	} else {
+		resetGame()
+		localStorage["checkResult"] = ""
+	}
+	redraw()
 }
+
 
 M.AutoInit();
 setup();
